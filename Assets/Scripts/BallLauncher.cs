2c364da5a237e3e7f497b0a672069d4bea064893
Bryan <bryancrim@gmail.com>
2020-02-25 17:46:47 +0100

    BallLauncher & BallReaction Scripts added

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour
+{
+    public Transform launchPoint;
+    public GameObject ballPrefab;
+
+    float bulletForce = 10f;
+
+    private bool canLaunch = true;
+
+    //public AudioSource bulletSound;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && canLaunch)
+        {
+            //bulletSound.Play();
+            Shoot();
+            canLaunch = false;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(ballPrefab, launchPoint.position, launchPoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(launchPoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
