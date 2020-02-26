3e186b304161dc9de19e8114fd765a771519071f
Bryan <bryancrim@gmail.com>
2020-02-26 14:27:31 +0100

    trianglePrefab + CheckWin script added

---
+++ Assets/Scripts/CheckWin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckWin : MonoBehaviour
+{
+
+
+    public string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "ball")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
